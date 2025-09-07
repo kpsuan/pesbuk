e512609ca09f4b80bc8e598e15ce12149ee3d76a
Kismet Suan <kpsuan@up.edu.ph>
2025-09-07 19:32:02 +0800

    Add app routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,33 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+devise_for :users
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+resources :users, only: [:index, :show] do
+resources :follow_requests, only: [:create]
+end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
 
-  if Rails.env.development?
-    mount LetterOpenerWeb::Engine, at: "/letter_opener"
-  end
+resources :follow_requests, only: [:index, :destroy] do
+member do
+patch :accept
+patch :decline
+end
+end
+
+
+resources :follows, only: [:destroy]
+
+
+resources :posts do
+resources :comments, only: [:create, :destroy]
+resource :like, only: [:create, :destroy]
+end
+
+
+root 'posts#index'
+
+
+if Rails.env.development?
+mount LetterOpenerWeb::Engine, at: "/letter_opener"
+end
 end
